0d621648d01256d71ab5b46465cd9d48d69f85ce
Olaf Kober <olaf.kober@outlook.com>
2018-06-16 10:04:10 +0200

    added benchmarking project

---
+++ src/Benchmark.Amarok.Events/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Copyright(c) 2018, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Events
+ */
+
+using System;
+
+
+namespace Amarok
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
