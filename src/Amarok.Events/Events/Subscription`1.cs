f84d79f9bc116e8e7859a703ea7a9d307a5d35ea
Olaf Kober <olaf.kober@outlook.com>
2018-06-22 23:20:40 +0200

    .

--- src/Amarok.Events/Events/Subscription`1.cs
+++ src/Amarok.Events/Events/Subscription`1.cs
@@ -7,15 +7,33 @@
 #pragma warning disable S3881 // "IDisposable" should be implemented correctly
 
 using System;
+using System.Threading.Tasks;
 
 
 namespace Amarok.Events
 {
+	/// <summary>
+	/// Implementation class that represents a subscription.
+	/// 
+	/// Concrete derivations implement specific behaviors, for example, support for synchronous and 
+	/// asynchronous handler methods, weak subscriptions, etc.
+	/// </summary>
 	internal abstract class Subscription<T> :
 		IDisposable
 	{
+		/// <summary>
+		/// Invokes the subscription's handler in a synchronous way.
+		/// </summary>
 		public abstract void Invoke(T value);
 
+		/// <summary>
+		/// Invokes the subscription's handler in a asynchronous way.
+		/// </summary>
+		public abstract ValueTask InvokeAsync(T value);
+
+		/// <summary>
+		/// Disposes the subscription; removes it from the event source.
+		/// </summary>
 		public abstract void Dispose();
 	}
 }
