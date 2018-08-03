8254c6af3cd3c038654695b32d648fdc95776530
Olaf Kober <olaf.kober@outlook.com>
2018-08-03 10:49:52 +0200

    mark assembly as CLS-compliant

--- src/Amarok.Events/AssemblyInfo.cs
+++ src/Amarok.Events/AssemblyInfo.cs
@@ -3,8 +3,17 @@
  * https://github.com/Amarok79/Events
  */
 
+using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+
+// Assembly Configuration
+[assembly: CLSCompliant(true)]
+
+// COM Configuration
+[assembly: ComVisible(false)]
+[assembly: Guid("E20AE02F-C5DB-4928-B622-4AF5A0B83145")]
 
 // Friend Test Assemblies
 [assembly: InternalsVisibleTo("Tests.Amarok.Events, PublicKey=0024000004800000940000000602000000240000525341310004000001000100691420b75c6bd08d4cde644c991ae9f5d0739ac5dc6dd6a7428fbfe03c292dae8cc9eece727e62fbbe59ef392c61ecafe563b96484e28def27eae70aff042cecaa2cfff56fed30fe6fbed507b8f159f4a43608f284a9186b8aeffd25ce944dcae0c14f61e00fe997cdf28dfa0824a65ab491aa7caddd233e6faceecadfc80eb3")]
