438851249f1615ca82989984691b09866e71d336
Olaf Kober <olaf.kober@outlook.com>
2018-11-01 19:53:01 +0100

    adapted source file headers to changed project name

--- src/Benchmark.Amarok.Events/Events/Program.cs
+++ src/Benchmark.Amarok.Events/Events/Program.cs
@@ -1,6 +1,6 @@
 ﻿/* Copyright(c) 2018, Olaf Kober
  * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
- * https://github.com/Amarok79/Events
+ * https://github.com/Amarok79/Amarok.Events
  */
 
 using BenchmarkDotNet.Running;
--- src/Tests.Amarok.Events/Events/Test_EventSource_AsyncHandler.cs
+++ src/Tests.Amarok.Events/Events/Test_EventSource_AsyncHandler.cs
@@ -1,6 +1,6 @@
 ﻿/* Copyright(c) 2018, Olaf Kober
  * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
- * https://github.com/Amarok79/Events
+ * https://github.com/Amarok79/Amarok.Events
  */
 
 #pragma warning disable S3257 // Declarations and initializations should be as concise as possible
