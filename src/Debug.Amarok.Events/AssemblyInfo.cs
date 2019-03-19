efc579aa9f366e037a76e551e125abbad8bfaf4f
Olaf Kober <olaf.kober@outlook.com>
2019-03-19 13:39:41 +0100

    reviewed debug app project settings

---
+++ src/Debug.Amarok.Events/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Events
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+
+// Assembly Configuration
+[assembly: CLSCompliant(true)]
+
+// COM Configuration
+[assembly: ComVisible(false)]
+[assembly: Guid("7BED50BB-F937-4ED8-96FE-34C9ED009716")]
---
+++ src/Debug.Amarok.Events/Events/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Events
+ */
+
+using System;
+
+
+namespace Amarok.Events
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			// Method intentionally left empty.
+		}
+	}
+}
