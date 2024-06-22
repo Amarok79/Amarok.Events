2f1d1c10190a4f68ba0c16123c161357b4aa448f
Olaf Kober <olaf.kober@outlook.com>
2024-06-22 20:51:23 +0200

    Refactor projects by moving common settings out into Directory.Build.props

---
+++ src/Benchmarks.Amarok.Events/Events/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2022, Olaf Kober <olaf.kober@outlook.com>
+
+using BenchmarkDotNet.Running;
+
+
+namespace Amarok.Events;
+
+
+public static class Program
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<Benchmarks>();
+    }
+}
